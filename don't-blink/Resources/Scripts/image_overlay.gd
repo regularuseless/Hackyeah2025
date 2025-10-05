bb537a5cf0ada17cab462805748a5e17d36ac3f3
flipolipo <juroszekfilip123@gmail.com>
2025-10-05 04:04:03 +0200

    interact with item

--- don't-blink/Resources/Scripts/image_overlay.gd
+++ don't-blink/Resources/Scripts/image_overlay.gd
@@ -6,7 +6,7 @@ class_name ImageOverlay
 
 func _ready() -> void:
 	visible = false
-	mouse_filter = Control.MOUSE_FILTER_STOP  # łap klik, nie przepuszczaj do gry
+	mouse_filter = Control.MOUSE_FILTER_STOP  
 
 func show_image(tex: Texture2D) -> void:
 	if tex == null: 
@@ -18,14 +18,14 @@ func close() -> void:
 	visible = false
 	picture.texture = null
 
-# kliknięcie gdziekolwiek w overlay zamyka
+
 func gui_input(event: InputEvent) -> void:
 	if not visible: 
 		return
 	if event is InputEventMouseButton and event.pressed:
 		close()
 
-# Esc też zamyka
+
 func _unhandled_input(event: InputEvent) -> void:
 	if not visible:
 		return
--- don't-blink/Resources/Scripts/journal.gd
+++ don't-blink/Resources/Scripts/journal.gd
@@ -1,12 +1,13 @@
+# Journal.gd
 extends Area2D
 
-@export var picture_to_show: Texture2D     # obrazek, który ma się wyświetlić na ekranie
-@export var overlay_path: NodePath         # ścieżka do węzła ImageOverlay
+@export var picture_to_show: Texture2D 
+@export var overlay_path: NodePath 
 
 var overlay: ImageOverlay = null
 
 func _ready() -> void:
-	input_pickable = true  # dla pewności
+	input_pickable = true  
 	if overlay_path != NodePath():
 		overlay = get_node_or_null(overlay_path) as ImageOverlay
 	if overlay == null:
